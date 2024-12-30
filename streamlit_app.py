5991c63e7ebbf05cc2da2750925a97f84aacd9b1
anant <anant@datatecnica.com>
2024-12-30 18:55:48 +0530

    cellpose v2 results

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,38 @@ from glob import glob
 import pickle 
 from streamlit_image_annotation import detection
 import json
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
-root_folder = "Visualization_DAPI"
+root_folder = "Visualization_DAPI_INSPECTION"
+root_folder = "Dec29_60X_OME-TIFFs_Visualization_DAPI_INSPECTION"
 
 marker_set = os.listdir(root_folder)
 # col3.title("DAPI Segmented Image")
 # col4.title("DAPI Raw Image")
 
-mset = st.selectbox("Select marker set", marker_set)
+indipl = """INDI00002D
+INDI00003D
+INDI00004D
+INDI00005D
+INDI00006D
+INDI00007D
+INDI00008D
+INDI00009D
+INDI00010D
+INDI00011D
+INDI00019D
+INDI00012D
+INDI00013D
+INDI00014D
+INDI00015D
+INDI00016D
+INDI00017D
+INDI00018D"""
+
+selectedplate = st.selectbox("Select plate", indipl.split("\n")) # ["INDI00003D", "INDI00004D", "INDI00005D", "INDI00007D"]
+
+
+# mset = st.selectbox("Select marker set", marker_set)
 
 
 image_path_list = []
@@ -26,13 +49,14 @@ actual_path_list = []
 all_image_path_list = []
 for msett in marker_set:
     list_of_image_folders = os.listdir(root_folder  + "/" + msett)
-    for img_folder in list_of_image_folders[:20]:
+    for img_folder in list_of_image_folders: # [:20]:
         fullpath = root_folder  + "/" + msett + "/" + img_folder
-        all_image_path_list.append(fullpath + '/actualImage.png')
-        if mset == msett:
+        if selectedplate in fullpath:
+            all_image_path_list.append(fullpath + '/actualImage.png')
+        if selectedplate in fullpath:
             image_path_list.append(fullpath + '/actualImage.png')
             actual_path_list.append(fullpath + '/segmentedImage.png')
-        
+
 
 
 if True:
@@ -60,28 +84,12 @@ if True:
     num_page = st.slider('page', 0, len(image_path_list)-1, 0, key='slider')
     # for num_page, img_folder in enumerate(list_of_image_folders):
     if True:
-        col3, col4 = st.columns([1, 2])
-        col3.header("Dapi segmented image")
-        col4.header("Dapi raw image")
+        col3, col4 = st.columns([1, 1])
+        col4.write("Dapi segmented image")
+        col3.write(os.path.basename(Path(actual_path_list[num_page]).parent))
         target_image_path = image_path_list[num_page]
-        col3.image(actual_path_list[num_page])
-        with col4:
-            new_labels = detection(image_path=target_image_path, 
-                                bboxes=st.session_state['result_dict'][target_image_path]['bboxes'], 
-                                labels=st.session_state['result_dict'][target_image_path]['labels'], 
-                                label_list=label_list, use_space=True, key=target_image_path)
-            if new_labels is not None:
-                st.session_state['result_dict'][target_image_path]['bboxes'] = [v['bbox'] for v in new_labels]
-                st.session_state['result_dict'][target_image_path]['labels'] = [v['label_id'] for v in new_labels]
-
-
-            with open("result_dict.json", "r") as outfile: 
-                result_dict_exist = json.load(outfile)
-            
-            if not json.dumps(result_dict_exist) == json.dumps(st.session_state['result_dict']):
-                # Convert and write JSON object to file
-                with open("result_dict.json", "w") as outfile: 
-                    json.dump(st.session_state['result_dict'], outfile)
+        col4.image(actual_path_list[num_page])
+        col3.image(image_path_list[num_page])
 
     
     # st.json(st.session_state['result_dict'])
